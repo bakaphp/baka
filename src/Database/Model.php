dddab08d56a81e0e65b3ead763bfad628ab7b29f
FredPeal <frederickpeal@gmail.com>
2021-01-15 16:28:14 -0400

    Improve get relations

--- src/Database/Model.php
+++ src/Database/Model.php
@@ -341,8 +341,6 @@ class Model extends PhalconModel implements ModelInterface, PhalconModelInterfac
      */
     public function getRelations() : array
     {
-        $class = get_class($this);
-
-        return $this->getModelsManager()->getRelations($class);
+        return $this->getModelsManager()->getRelations(get_class($this));
     }
 }
