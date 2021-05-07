bb55e8bbeb9442a8c87ccc1f1a7c4dc1751e4b6f
kaioken <max@mctekk.com>
2021-05-07 04:57:47 -0400

    Improve Elastic Additional Params

--- src/Http/QueryParser/NestedParenthesesParser.php
+++ src/Http/QueryParser/NestedParenthesesParser.php
@@ -161,10 +161,18 @@ class NestedParenthesesParser
     {
         $newAdditionalQueryFields = [];
 
+        /**
+         * Additional Params
+         * ['follows_rs.users_id', ':', $this->userData->getId()],
+         *  0 -> key
+         *  1 -> operation
+         *  2 -> value
+         *  3 -> join operator.
+         */
         foreach ($additionalQueryFields as $query) {
             $newAdditionalQueryFields[$query[0]] = [
-                'comparison' => implode('', $query),
-                'joiner' => ',',
+                'comparison' => implode('', array_slice($query, 0, 3)),
+                'joiner' => !is_null($query[3]) && QueryParser::isAValidJoiner($query[3]) ? $query[3] : ',',
                 'key' => $query[0]
             ];
         }
