e0a56748aedd4e4be420ab209ebca76a8da3d467
kaioken <max@mctekk.com>
2021-05-23 19:54:53 -0400

    udpate

--- src/Http/Converter/RequestUriToSql.php
+++ src/Http/Converter/RequestUriToSql.php
@@ -966,9 +966,10 @@ class RequestUriToSql extends Injectable implements ConverterInterface
 
     /**
      * Allow use to overwrite the sort , since its manual it can be only run by the user
-     * so he has to avoid the sql injection
+     * so he has to avoid the sql injection.
      *
      * @param string|null $sort
+     * 
      * @return void
      */
     public function setManualCustomSort(?string $sort) : void
